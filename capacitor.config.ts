e53c26bf345a3fa52d46c9f2399e8bfeec2c007f
nitrotap <9121718+nitrotap@users.noreply.github.com>
2023-03-30 19:08:29 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'line-mash',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
